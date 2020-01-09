2a9c2e0ca0f08bb3bca61f286bd64972eaa6311f
msmendoza <msmendoza@uc.cl>
2020-01-09 17:46:45 -0300

    Vacantes reservadas

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ Pontifical Catholic University of Chile
 
 """
 
-import requests
+import requests as req
 from bs4 import BeautifulSoup
 
 
@@ -23,7 +23,7 @@ def vacantes(sigla, sec, url):
     """
 
     url = url.format(sigla)
-    response = requests.get(url=url)
+    response = req.get(url=url)
     soup = BeautifulSoup(markup=response.text, features='html5lib')
 
     filtered_tr_tags = [i for i in soup.find_all(name='tr') if len(i) == 33]
@@ -40,4 +40,19 @@ def vacantes(sigla, sec, url):
 
 
 if __name__ == '__main__':
-    pass
+    # TODO
+    #       - Investigar oneclick fields en <a> tags y como obtener información
+    #           a partir de ellos (Ejemplo de ICS3153 abajo)
+
+    """
+    onclick="javascript:test('./informacionVacReserva.ajax.php?
+    nrc=14135&
+    termcode=2020-1&
+    cantidad_dis=30&
+    cantidad_min=30&
+    cantidad_ocu=0&
+    nombre=Optimizaci%C3%B3n+Avanzada&
+    sigla=ICS3153&
+    seccion=1
+    ');"
+    """
