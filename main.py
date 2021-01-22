e79871c1c71dd22566be14158bb78dffc25aa648
msmendozael <msmendoza@uc.cl>
2021-01-22 11:24:12 -0300

    Cambio de NRCs

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
 from functions import run
 
-semestre = '2020-2'
+semestre = '2021-1'
 ramos = [
-    # {'nrc': 20437, 'sigla': 'ICS3105', 'sec': 1},
-    # {'nrc': 11114, 'sigla': 'DPT5502', 'sec': 3},
-    # {'nrc': 10270, 'sigla': 'DPT9330', 'sec': 1},
-    # {'nrc': 24618, 'sigla': 'LET061I', 'sec': 1},
-    # Major/Título
-    # A
-    {'nrc': 13579, 'sigla': 'ICS3413', 'sec': 1},  # Finanzas
-    # B
-    # {'nrc': 12357, 'sigla': 'IIC2133', 'sec': 1},  # EDD 1
-    # {'nrc': 24358, 'sigla': 'IIC2133', 'sec': 2},  # EDD 2
-
-    # OFGs
-    # A
-    {'nrc': 17865, 'sigla': 'CAR1500', 'sec': 1},  # Ent Presentaciones O.
-    {'nrc': 12426, 'sigla': 'LET056E', 'sec': 1},  # Catalan
-    {'nrc': 20536, 'sigla': 'LET218E', 'sec': 1},  # Lit Japonesa
-
-    # B
-    {'nrc': 13477, 'sigla': 'CAR0004', 'sec': 1},  # Taller Tutores
-    # {'nrc': 14518, 'sigla': 'GEO111', 'sec': 1},  # Geografía de Chile: E y S
-
-    # C
-
-    # Ya tomado
-    # {'nrc': 15993, 'sigla': 'MUC1000', 'sec': 1},
-    # {'nrc': 14352, 'sigla': 'ICS2813', 'sec': 1},
-    # {'nrc': 16922, 'sigla': 'ICS2122', 'sec': 1},
-
+    # Pregrado
+    {'nrc': 15218, 'sigla': 'ICS3313', 'sec': 3},
+    {'nrc': 10207, 'sigla': 'IHI2315', 'sec': 1},
+    {'nrc': 10725, 'sigla': 'IIC2133', 'sec': 1},
+    {'nrc': 10732, 'sigla': 'IIC2143', 'sec': 1},
 ]
 
 if __name__ == '__main__':
     run(semestre, ramos, interval=60)
-    pass
