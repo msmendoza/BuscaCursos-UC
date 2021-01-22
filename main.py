1a37d2815e3c46f40fe80580b228f256eb40cc10
msmendozael <msmendoza@uc.cl>
2021-01-22 11:48:27 -0300

    Cambio de NRCs II

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ ramos = [
     {'nrc': 10207, 'sigla': 'IHI2315', 'sec': 1},
     {'nrc': 10725, 'sigla': 'IIC2133', 'sec': 1},
     {'nrc': 10732, 'sigla': 'IIC2143', 'sec': 1},
+
+    # Práctica II
+    {'nrc': 24183, 'sigla': 'ING2005', 'sec': 1},
+    {'nrc': 11844, 'sigla': 'ING2001', 'sec': 1},
+
+    # Magíster
+    {'nrc': 18871, 'sigla': 'IIC3697', 'sec': 1},
+    {'nrc': 10930, 'sigla': 'IIC3724', 'sec': 1},
+    {'nrc': 11856, 'sigla': 'ING4921', 'sec': 1},
 ]
 
 if __name__ == '__main__':
